86eaeeeb8b78a920c87386ab90d5c26380a84820
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-16 12:14:48 -0700

    add difference method test, fails.

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,8 +1,19 @@
 import { Age } from './../src/age.js';
 
 describe('Age', function() {
+
   it('should convert age from years to seconds', function() {
-    var age = new Age;
+    let age = new Age;
     expect(age.seconds(2)).toEqual(63115200);
+  });
+
+  it('should return the difference between 2 dates in seconds', function() {
+    let yesterday = new Date(2018, 2, 15);
+    let today = new Date(2018, 2, 16);
+    let yesterdaySeconds = yesterday.getTime() / 1000;
+    let todaySeconds = today.getTime() / 1000;
+    expect(difference(yesterday, today)).toEqual(86400)
+
   })
+
 })
--- src/age.js
+++ src/age.js
@@ -5,4 +5,8 @@ export class Age {
     return seconds;
   }
 
+  difference(dob, now) {
+    
+  }
+
 }
